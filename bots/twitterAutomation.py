11bb2cf4aa1191dd611608a7b444bb7184afd3ca
danieldewaard <dbde3@student.monash.edu>
2020-02-20 23:42:51 +1100

    Final tweets for meme posting prod

--- bots/twitterAutomation.py
+++ bots/twitterAutomation.py
@@ -32,32 +32,28 @@ def twitter_authentication():
 
 def load_memes():
     
+    logger.info("Loading memes")
+    
     basepath = "../photos/memes/modified/"
     memes = []
     with os.scandir(basepath) as entries:
         for entry in entries:
             if entry.is_file():
-                print(entry.name)
+                #print(entry.name)
                 memes.append(entry.name)
+    
+    logger.info(str(len(memes))+" memes loaded")
     
     return memes
 
-def twitter_automation(api, memes): 
+def twitter_automation(api): 
     
     global i
     i = i + 1
-    max_memes_address = len(memes)-1
-    
-    rand_meme = random.randrange(0,max_memes_address)
-    
-    for k in range(0,max_memes_address):
-        print(k)
-        api.update_with_media("../photos/memes/modified/"+memes[k])
     
     #sourcing the weather
     response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Taipei&units=metric&appid="+os.getenv("weatherAPI_key"))
     response = json.loads(response.text)
-
     weather = response["weather"][0]["main"]
     weather_detailed = (response["weather"][0]["description"]).lower()
     temperature = str(round(response["main"]["temp"],1))
@@ -76,22 +72,38 @@ def twitter_automation(api, memes):
         rain_hashtag = "#norain"
 
     logger.info("rain status: "+str(rain_flag))    
-
-    #updating the twitter status    
-    try:
-        status = api.update_status(rain_status + "\nCurrent temperature: "+temperature+" degrees \nCurrent weather: "+weather_detailed+" \n#Taipei #Taiwan #Weather " + rain_hashtag +" #"+str(i))
-    except Exception as e:
-        logger.error("Error posting tweet", exc_info=True)
-        raise e
+    
+    if rain_flag == 0:
+    
+        #updating the twitter status    
+        try:
+            status = api.update_status(rain_status + "\nCurrent temperature: "+temperature+" degrees \nCurrent weather: "+weather_detailed+" \n#Taipei #Taiwan #weather " + rain_hashtag +" #")
+            logger.info("Tweet posted, it is not raining")
+        except Exception as e:
+            logger.error("Error posting tweet", exc_info=True)
+            raise e
+            
+    if rain_flag == 1: 
+        
+        memes = load_memes()
+        
+        max_memes_address = len(memes)-1
+        rand_meme = random.randrange(0,max_memes_address)
+        
+        #updating the twitter status    
+        try:
+            status = api.update_with_media("../photos/memes/modified/"+memes[rand_meme],rain_status + "\nCurrent temperature: "+temperature+" degrees \nCurrent weather: "+weather_detailed+" \n#Taipei #Taiwan #weather " + rain_hashtag +" #memes")
+            logger.info("Tweet posted, it is raining and a meme was also posted")
+        except Exception as e:
+            logger.error("Error posting tweet", exc_info=True)
+            raise e
     
 def main():
-    
-    memes = load_memes()
     
     api = twitter_authentication()
     
     while True:
-        twitter_automation(api, memes)
+        twitter_automation(api)
         time.sleep(3600)
         
 if __name__ == "__main__":
